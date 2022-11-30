967e950e66781992ce3d4adfe9fc9b63aca298c9
Amal-2020 <123ask4amal@gmail.com>
2022-11-30 22:02:44 +0530

    even_odd_sum

--- src/JAVA/Even_Odd_sum.java
+++ src/JAVA/Even_Odd_sum.java
@@ -1,2 +1,36 @@
-package JAVA;public class Even_Odd_sum {
+package JAVA;
+
+import java.util.Scanner;
+
+public class Even_Odd_sum {
+    public static void main(String[] args) {
+        int n, i, sum = 0, sum1 = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the limit:");
+        n = sc.nextInt();
+
+        System.out.println("The Numbers are:");
+        for (i = 1; i <= n; i++) {
+            System.out.printf("%s ",i);
+        }
+        //print the element 1 to n
+        for (i = 1; i <= n; i++) {
+
+            //give the sum of even and odd numbers
+            if (i % 2 == 0) {
+                int even = i;
+                sum = sum + i;
+            }
+            else {
+                int odd = i;
+                sum1 = sum1 + i;
+
+            }
+        }
+
+        System.out.println("\nEven number sum = " + sum);
+        System.out.println("Odd number sum = " + sum1);
+    }
 }
